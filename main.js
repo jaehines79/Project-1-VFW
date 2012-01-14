b37ba01a3467d319349d569c3379a2ca8a45e812
Janis Hines <jaehines79@fullsail.edu>
2012-01-13 22:39:27 -0500

    Finished Project2

--- main.js
+++ main.js
@@ -5,24 +5,23 @@
 
 
 window.addEventListener("DOMContentLoaded", function(){
-	//alert(localstorage.value(0));
 	
 	//getElementbyId Function
 	function $(x){
 		var theElement = document.getElementById(x);
 		return theElement;
-		}
+	}
 		
-	//Create select field element and populate with functions
+	//Create select field element and populate with options
 	function makeCats(){
-		var formTag = document.getElementByTagName("form"); //formTag is an array of all the formTags
+		var formTag = document.getElementsByTagName("form"),
 			selectLi = $("select"),
 			makeSelect = document.createElement("select");
-			makeSelect.setAttribute("id", "groups");
-		for(var i=0, j=eventGroups.length; i<j; i++){
-			var makeOption = document.creatElement("option");
-			var optText = eventGroups[i];
-			makeOption.setAttribute("value", optText);
+			makeSelect.setAttribute("id", "eventgroup");
+		for( var i=0, j = eventGroup.length; i < j; i++ ) {
+			var makeOption = document.createElement("option");
+			var optText = eventGroup[i];
+			makeOption.setAttribute("value", eventGroup[i]);
 			makeOption.innerHTML = optText;
 			makeSelect.appendChild(makeOption);
 		}
@@ -39,40 +38,94 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('eventform').style.display = "none";
+				$('clearlink').style.display = "inline";
+				$('displayLink').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('eventform').style.display = "block";
+				$('clearlink').style.display = "inline";
+				$('displayLink').style.display = "inline";
+				$('addNew').style.display = "none";
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
 	function storeData(){
-		var id    =Math.floor(Math.random()*100000001);
+		var id    = Math.floor(Math.random()*10000001);
 		//gather form field values in an object
 		//object properties contain array with form label and input values
-		getSelectedRadio();
 		var item      	    = {};
-			item.group		= ["Group: ", $("groups"), value];
-			item.ename 		= ["Name: ", $("name"), value];
-			item.edate		= ["Date: ", $("date"), value];
+			item.ename 		= ["Name: ", $('name').value];
+			item.edate		= ["Date: ", $('date').value];
 			item.relation	= ["Choice: ", relationvalue];
-			item.notes		= ["Notes: ", $("comments"), value];
-			item.importance = ["Importance: ", $("importance"), value];
-			
-			
+			item.importance = ["Importance: ", $('importance').value];
+			item.eventtype 	= ["Event Type: ", $('eventtype').value];
+			item.notes		= ["Notes: ", $('comments').value];
 		//Save data into local storage: Use Stringify to convert object to a string
 		localstorage.setItem(id, JSON.stringify(item));
 		alert("Event Saved");
+	}	
 		
-		
-	
+	function getData(){
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no data in Local Storage.");
+		};
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "display";
+		for(var i=0, len = localStorage.length; i<len; i++){
+			var makeli = document.createElement('li');
+			makeList.appendChild(makeList);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//covert string from local storage value back to an object 
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeli.appendChild(makeSubList);
+			for (var n in obj){
+				var makeSubLi = document.createElement('li');
+				makeSubLi.appendChild(makeSubLi);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+		}
+	}
+}
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.");
+		}else{
+			localStorage.clear();
+			alert("All events have been deleted!");
+			window.location.reload();
+			return false;
+		}
+	}
 
 	//Variable Defaults
-	var eventGroups = ["--Choose a Group--", "Friends", "Family", "Work"];
+	var eventGroup = ["--Choose an Event Type--", "Birthday", "Anniversary", "Holiday", "Other"],
 		relationValue
-		favoriteValue = "No"
+	;
 	
-	makecats();
+	makeCats();
 	
 	//Set Link & Submit Click Events
-	var displayLink = $("display");
-	displayLink.addeventListener("click", getData);
-	var clearLink = $("clear");
+	var displayLink = $('displayLink');
+	displayLink.addEventListener("click", getData);
+	var clearlink = $('clearlink');
 	clearlink.addEventListener("click", clearLocal);
-	var save = $("submit");
+	var save = $('submit');
 	save.addEventListener("click", storeData);
 }
-})
+);
